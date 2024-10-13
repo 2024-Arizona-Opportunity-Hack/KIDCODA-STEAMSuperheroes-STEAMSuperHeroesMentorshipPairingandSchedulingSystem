9f0a5e39660cadd324f51c6d253ac29858621fb9
hariprakash619 <hariprakash.619@gmail.com>
2024-10-13 11:40:20 -0700

    changed the ethnicity function

--- app/import_data.py
+++ app/import_data.py
@@ -6,18 +6,93 @@ import io
 # Initialize DynamoDB resource
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')  # Specify your region
 
+academic_level_mapping = {
+    "5th grade": 5,
+    "6th grade": 6,
+    "7th grade": 7,
+    "8th grade": 8,
+    "9th grade": 9,
+    "10th grade": 10,
+    "11th grade": 11,
+    "12th grade": 12,
+    "College Freshman": 13,
+    "College Sophomore": 14,
+    "College Junior": 15,
+    "College Senior": 16,
+    "Graduate Student": 17,
+    "High School Freshman": 9,
+    "High School Sophomore": 10,
+    "High School Junior": 11,
+    "High School Senior": 12,
+    "College Undergraduate": 13,
+    "Graduate School": 17,
+    "Graduated / Working Professional": 18
+}
+
+ethnicity_mapping = {
+    "South Asian: Includes Indian, Pakistan, Sri Lankan, Bangaladesh": 1,
+    "Black or African American: Includes Jamaican, Nigerian, Haitian, and Ethiopian": 2,
+    "White or European: Includes German, Irish, English, Italian, Polish, and French": 3,
+    "Hispanic or Latino: Includes Puerto Rican, Mexican, Cuban, Salvadoran, and Colombian": 4,
+    "Middle Eastern or North African: Includes Lebanese, Iranian, Egyptian, Moroccan, Israeli, and Palestinian": 5,
+    "Native Hawaiian or Pacific Islander: Includes Samoan, Buamanian, Chamorro, and Tongan": 6,
+    "American Indian or Alaska Native": 7,
+    "Asian: Includes Chinese, Japanese, Filipino, Korean, South Asian, and Vietnamese": 8
+}
+
+# Map preferences to numbers (1-5)
+preference_mapping = {
+    "Prefer ONLY to be matched within that similarity": 1,
+    "Prefer it, but available to others as needed": 2,
+    "Prefer NOT to be matched within that similarity": 3,
+    "Do not have a preference. Either is fine.": 4,
+    "Other:": 5
+}
+
+gender_mapping = {"Cisgender Male": 1, "Cisgender Female": 2, 
+                  "Transgender Male":3, "Transgender Female":4, 
+                  "Prefer not to disclose":5, "Others":6
+}
+
 # References to DynamoDB tables
 users_table = dynamodb.Table('Users')
+mentors_table = dynamodb.Table('Mentors')
+mentees_table = dynamodb.Table('Mentees')
 
-# Function to transform session types into the desired format
 def transform_session_types(session_types_str):
-    session_types = session_types_str.split(';')
-    session_data = [{"Session Type": session_type.strip(), "isSessionMatched": False} for session_type in session_types]
+    session_types = session_types_str.split(',')
+    session_data = [{"type": session_type.strip(), "is_match_found": False} for session_type in session_types]
     return session_data
+
+def get_state_abbreviation(state_str):
+    state_name = state_str.split(":")[0].strip()
+    return state_name
+
+def map_academic_level(level_str):
+    return academic_level_mapping.get(level_str, 0)
+
+def map_ethnicity(ethnicity_str):
+    ethnicities = [ethnicity_mapping[ethnicity] for ethnicity in ethnicity_mapping if ethnicity in ethnicity_str]
+    return ethnicities
+    
+def map_gender(gender):
+    return [gender_mapping.get(g.strip(), 0) for g in gender.split(",")]
+
+def map_preference(preference_str):
+    return preference_mapping.get(preference_str, 4)
+
+def get_age_average(age_str):
+    if "+" in age_str:
+        return 70
+    elif "-" in age_str:
+        age_range = age_str.split("-")
+        return (int(age_range[0]) + int(age_range[1])) // 2
+    else:
+        return int(age_str) 
 
 def insert_user(user_data):
     try:
-        print(f"Inserting into {users_table.name}: {user_data}")  # Debug statement
+        print(f"Inserting into {users_table.name}: {user_data}")
         users_table.put_item(Item=user_data)
         print(f"Successfully inserted user: {user_data['PK']}, {user_data['SK']}")
     except ClientError as e:
@@ -32,15 +107,15 @@ def insert_mentor(user_data):
         "AvailableTimes": user_data["AvailableTimes"],
         "SessionTypes": user_data["SessionTypes"],
         "Email": user_data["Email"],
-        "Age": user_data["Age"],
-        "Gender": user_data["Gender"],
-        "Ethnicity": user_data["Ethnicity"],
-        "EthnicityPref": user_data["EthnicityPref"],
-        "GenderPref": user_data["GenderPref"],
+        "Age": get_age_average(user_data["Age"]),
+        "Gender": map_gender(user_data["Gender"]),
+        "Ethnicity": map_ethnicity(user_data["Ethnicity"]),
+        "EthnicityPref": map_preference(user_data["EthnicityPref"]),
+        "GenderPref":map_preference(user_data["EthnicityPref"]),
         "LocationCity": user_data["LocationCity"],
         "MentoringMethods": user_data["MentoringMethods"],
-        "LocationState": user_data["LocationState"],
-        "AcademicLevel": user_data["AcademicLevel"]
+        "LocationState": get_state_abbreviation(user_data["LocationState"]),
+        "AcademicLevel": map_academic_level(user_data["AcademicLevel"])
     }
     try:
         print(f"Inserting into {mentors_table.name}: {mentor_data}")  # Debug statement
@@ -50,24 +125,26 @@ def insert_mentor(user_data):
         print(f"Error inserting mentor: {e.response['Error']['Message']}")
 
 def insert_mentee(user_data):
+    gender = map_gender(user_data["Gender"])
+    print("Gender : ", gender)
     mentee_data = {
         "PK": user_data["PK"],
         "SK": "MENTEE_PROFILE",
         "AvailableTimes": user_data["AvailableTimes"],
         "SessionTypes": user_data["SessionTypes"],
         "Email": user_data["Email"],
-        "Age": user_data["Age"],
-        "Gender": user_data["Gender"],
-        "Ethnicity": user_data["Ethnicity"],
-        "EthnicityPref": user_data["EthnicityPref"],
-        "GenderPref": user_data["GenderPref"],
+        "Age": get_age_average(user_data["Age"]),
+        "Gender": map_gender(user_data["Gender"]),
+        "Ethnicity": map_ethnicity(user_data["Ethnicity"]),
+        "EthnicityPref": map_preference(user_data["EthnicityPref"]),
+        "GenderPref": map_preference(user_data["EthnicityPref"]),
         "LocationCity": user_data["LocationCity"],
         "MentoringMethods": user_data["MentoringMethods"],
-        "LocationState": user_data["LocationState"],
-        "Grade": user_data["GradeLevel"]
+        "LocationState": get_state_abbreviation(user_data["LocationState"]),
+        "Grade": map_academic_level(user_data["GradeLevel"])
     }
     try:
-        print(f"Inserting into {mentees_table.name}: {mentee_data}")  # Debug statement
+        print(f"Inserting into {mentees_table.name}: {mentee_data}")
         mentees_table.put_item(Item=mentee_data)
         print(f"Successfully inserted mentee: {mentee_data['PK']}")
     except ClientError as e:
@@ -100,7 +177,7 @@ def lambda_handler(event, context):
             # Prepare the base user object
             user_data = {
                 "PK": f"USER#{row['Email']}",  # Using Email as unique PK
-                "SK": "MENTEE_PROFILE" if row["Choose the role you are signing up for"] == "Mentee" else "MENTOR_PROFILE",
+                "SK": "MENTEE_PROFILE" if row["Choose the role you are signing up for"].split(" ")[0] == "Mentee" else "MENTOR_PROFILE",
                 "Email": row["Email"],
                 "Name": row["Name (First, Last)"],
                 "Age": row["Age"],
@@ -108,9 +185,9 @@ def lambda_handler(event, context):
                 "LocationCity": row["Location: City"],
                 "LocationState": row["Location: State"],
                 "SessionTypes": session_types,  # Insert transformed session types here
-                "Ethnicity": row["Ethnicity"].split(','),
+                "Ethnicity": row["Ethnicity"],
                 "EthnicityPref": row["What is your preference in being matched with a person of the same ethnicity?"],
-                "Gender": row["Gender"].split(','),
+                "Gender": row["Gender"],
                 "GenderPref": row["What is your preference regarding being matched with a person of the same gender identity?"],
                 "MentoringMethods": row["What methods of mentoring are you open to?"].split(','),
                 "Role": row["Choose the role you are signing up for"].split(" ")[0],
@@ -131,7 +208,7 @@ def lambda_handler(event, context):
                     "Friday": [time.strip() for time in row["What day/times are good for regular meetings? (Must choose at least 3) [Friday]"].split(",")],
                     "Saturday": [time.strip() for time in row["What day/times are good for regular meetings? (Must choose at least 3) [Saturday]"].split(",")],
                     "Sunday": [time.strip() for time in row["What day/times are good for regular meetings? (Must choose at least 3) [Sunday]"].split(",")]
-    },
+                },
                 "UnavailableDates": row["What are specific date ranges that you are NOT available? Please enter in YYYYMMDD format with a comma between each unique date, and a - to specify a range."]
             }
 
